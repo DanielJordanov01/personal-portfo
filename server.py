726a884b120d267d232fab7bac0b6b815219445f
Даниел Валентинов <daniel159696@gmail.com>
2020-04-19 11:41:37 +0300

    added a db

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
@@ -18,17 +19,27 @@ def education():
 def contact():
 	return render_template('contact.html')
 
+def save_data_to_csv(data):
+	with open('database.csv', 'a+') as database2:
+		email = data['email']
+		subject = data['subject']
+		message = data['message']
+		csv_writer = csv.writer(database2, delimiter=',', quotechar=str(csv.QUOTE_NONE), quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
 	if request.method == 'POST':
 		try:
 			data = request.form.to_dict()
-			print(data)
-			return 'sent'
+			save_data_to_csv(data)
+			return render_template('thankyou.html')
 		except:
 			return 'Could not send. Check connection'
 	else:
 		return 'sth went wrong'
+
+
 
 if __name__ == '__main__':
 	app.run()
